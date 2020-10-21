787b782640efa15ac0117f47cdda7ee9c55f92b0
Shi Hao Hong <shihaohong94@gmail.com>
2020-10-21 22:49:20 +0800

    Test gen_l10n case

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Add this line.
 
 void main() {
-  runApp(MyApp());
+ runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     localizationsDelegates: AppLocalizations.localizationsDelegates,
+     supportedLocales: AppLocalizations.supportedLocales,
+     home: MyHomePage(title: 'Flutter Demo Home Page'),
+   );
+ }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -42,6 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(AppLocalizations.of(context).helloWorld),
+            Text(AppLocalizations.of(context).anotherHello),
+            Text(AppLocalizations.of(context).aThirdHello),
           ],
         ),
       ),
